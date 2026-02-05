5645390c5b431699789840ffbe69799802993be1
Rotem <rotemsinger2011@gmail.com>
2026-02-05 16:28:17 +0200

    Added assertions for responses

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -3,18 +3,16 @@ import { getRandomContactUsEmail } from '../utils/data-generator'
 import { test } from '../utils/fixtures'
 
 test("GET products", async ({ requestHandler }) => {
-  const response = await requestHandler.path("/catalog/api/v1/categories/4/products").getRequest(200)
+  const categoryId = 4
+  const response = await requestHandler.path(`/catalog/api/v1/categories/${categoryId}/products`).getRequest(200)
+  expect(response.categoryId).toEqual(categoryId)
 })
 
 test("POST contact us", async ({ requestHandler }) => {
-  // const body = {
-  //   "categoryId": 1,
-  //   "email": "rotem@mail.com",
-  //   "productId": 7,
-  //   "text": "ddsfsdsds"
-  // }
   const body = getRandomContactUsEmail()
-  console.log(body)
   const response = await requestHandler.path("/catalog/api/v1/support/contact_us/email").body(body).postRequest(200)
   await expect(response).isMatchingSchema("POST_support_contact_us_email_schema.json")
+  expect(response.success).toEqual(true)
+  expect(response.reason).toEqual("Thank you for contacting Advantage support.")
+  expect(response.returnCode).toEqual(1)
 })
