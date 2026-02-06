1b0fd27596ff3284e47d7dd0d4614efa49ad7802
Rotem <rotemsinger2011@gmail.com>
2026-02-06 16:25:00 +0200

    Added GET single product

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -1,18 +1,27 @@
 import { expect } from '../utils/custom-expect'
-import { getRandomContactUsEmail, getRandomCategoryAndProductIds } from '../utils/data-generator'
+import { getRandomContactUsEmail, getRandomCategoryAndProductIds as getRandomCategory } from '../utils/data-generator'
 import { test } from '../utils/fixtures'
 
 test("GET products", async ({ requestHandler }) => {
-  const { categoryId, categoryName } = getRandomCategoryAndProductIds()
-  const response = await requestHandler.path(`/catalog/api/v1/categories/${categoryId}/products`).getRequest(200)
-  await expect(response).isMatchingSchema("GET_products_schema.json")
-  expect(response.categoryId).toEqual(categoryId)
-  expect(response.categoryName).toEqual(categoryName.toUpperCase())
-  expect(response.categoryImageId).toEqual(categoryName.toLowerCase())
-  // Assert that all products in the "products" list have the correct category ID
-  response.products.forEach((product: { categoryId: number }) => {
+  const { categoryId, categoryName } = getRandomCategory()
+  const productsResponse = await requestHandler.path(`/catalog/api/v1/categories/${categoryId}/products`).getRequest(200)
+  console.log(productsResponse)
+  await expect(productsResponse).isMatchingSchema("GET_products_schema.json")
+  expect(productsResponse.categoryId).toEqual(categoryId)
+  expect(productsResponse.categoryName).toEqual(categoryName.toUpperCase())
+  expect(productsResponse.categoryImageId).toEqual(categoryName.toLowerCase())
+  // Assert that all products in the "products" key-value pair have the correct category ID
+  productsResponse.products.forEach((product: { categoryId: number }) => {
     expect(product.categoryId).toEqual(categoryId)
   })
+
+  // Select a random product from the Array
+  const selectedProduct = productsResponse.products[Math.floor(Math.random() * productsResponse.products.length)]
+  const singleProductResponse = await requestHandler.path(`/catalog/api/v1/products/${selectedProduct.productId}`).getRequest(200)
+  await expect(singleProductResponse).isMatchingSchema("GET_single_product_schema.json")
+  // console.log(singleProductResponse)
+  expect(singleProductResponse.productId).toEqual(selectedProduct.productId)
+  expect(singleProductResponse.categoryId).toEqual(selectedProduct.categoryId)
 })
 
 test("POST contact us", async ({ requestHandler }) => {
